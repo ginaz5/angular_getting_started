91e39ab41bafdab389f0e104ad09a278c4f46c05
Gina Lin <ginalin@alum.ccu.edu.tw>
2022-10-04 20:47:57 +0800

    add bootstrap, font, product component

---
+++ src/app/products/products-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'pm-products',
+  templateUrl: './products-list.component.html'
+})
+export class ProductsListComponent {
+
+}
