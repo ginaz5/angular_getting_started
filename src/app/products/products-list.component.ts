854f7be4c8437e8cfdb28813f82d1123b46481f0
Gina Lin <ginalin@alum.ccu.edu.tw>
2023-01-04 09:23:38 +0800

    two way binding

--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -9,6 +9,7 @@ export class ProductsListComponent {
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  listFilter: string = 'cart';
   products: any [] = [
     {
       "productId": 2,
