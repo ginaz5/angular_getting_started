b29699f8a4e37ac5687c1712d28ed55a80ec04c3
Gina Lin <ginalin@alum.ccu.edu.tw>
2023-01-22 10:27:46 +0800

    subscribe and unsubscribe observable

--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -1,17 +1,20 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, OnDestroy, OnInit} from "@angular/core";
 import {IProduct} from "./product";
 import {ProductService} from "./product.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'pm-products',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.css']
 })
-export class ProductsListComponent implements OnInit {
+export class ProductsListComponent implements OnInit, OnDestroy {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
+  errorMessage: string = '';
+  sub: Subscription | undefined;
 
   private _listFilter: string = '';
   get listFilter(): string {
@@ -39,8 +42,17 @@ export class ProductsListComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.products = this.productService.getProducts();
-    this.filteredProducts = this.products;
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  }
+
+  ngOnDestroy() {
+    this.sub?.unsubscribe();
   }
 
   onRatingClicked(message: string): void {
